0c11205090d1401927e58268720bdacd7ab6be73
Ashwin Arya <ashwin.arya@decurtis.com>
2022-06-29 09:08:13 +0530

    Array - Maximum product of first and last character of String after rotation https://www.geeksforgeeks.org/maximum-product-of-first-and-last-character-of-string-after-rotation/

---
+++ DSA-Rookie.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DSA_Rookie.BusinessLayer.Array;
+
+namespace DSA_Rookie.App
+{
+    class Program
+    {
+        private const string StringSample1 = "12345";
+
+        private const string StringSample2 = "86591";
+        
+        private readonly int[] IntArraySample1 = new int[] { 1, 8, 8, 4, 5 };
+
+        private readonly int[] IntArraySample2 = new int[] { 6, -2, -3, 4, -1, -2, 1, 5, -3 };
+
+        private const int NumberSample1 = 121251;
+
+        static void Main(string[] args)
+        {
+            MaximumProductOfFirtAndLastCharacter classA = new MaximumProductOfFirtAndLastCharacter();
+            classA.CalculateMaximumProduct(StringSample2);
+        }
+    }
+}
---
+++ DSA-Rookie.BusinessLayer/Array/MaximumProductOfFirtAndLastCharacter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA_Rookie.BusinessLayer.Array
+{
+    public class MaximumProductOfFirtAndLastCharacter
+    {
+        public void CalculateMaximumProduct(string str)
+        {
+            /* Input: 12345 -> Output: 5*4=20
+            Convert string to int[] - Parse string value to int to convert char to int
+            Multiple only [0] and [LastIndex]
+            Shift index of arrays after each multiplication.
+            Create variable to hold value of 1st index as loop is anticlockwise
+            Create variable to hold multiplied value            
+            */
+
+            // Converting string to int array for array manipulation
+            int[] convertedArray = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                int intValue;
+                int.TryParse(str[i].ToString(), out intValue);
+                convertedArray[i] = intValue;
+            }
+
+            int maxValue = 0;
+            // 1st loop required to calculate first and last index after each complete rotation 
+            for (int j = 0; j < str.Length; j++)
+            {
+                // 2nd loop multiplies first and last index and then shift values to previous index 
+                int firstValue = 0;
+                for (int i = 0; i < convertedArray.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        firstValue = convertedArray[i];
+                        int currentValue = convertedArray[0] * convertedArray[convertedArray.Length - 1];
+                        if (maxValue < currentValue)
+                        {
+                            maxValue = currentValue;
+                        }
+                    }
+
+                    if (i == convertedArray.Length - 1)
+                    {
+                        convertedArray[i] = firstValue;
+                    }
+                    else
+                    {
+                        convertedArray[i] = convertedArray[i + 1];
+                    }
+                }
+            }
+
+            Console.WriteLine("Maximum Product Of Firt And Last Character: " + maxValue);
+            Console.ReadKey();
+        }
+    }
+}
